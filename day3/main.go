231298b0a6efd88be63e001d89bcce93690753f7
Lcarv20 <42584819+Lcarv20@users.noreply.github.com>
2021-12-05 22:49:06 +0100

    Solving first part of the third day

--- day3/main.go
+++ day3/main.go
@@ -5,9 +5,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
+type Rates struct {
+	zeros int
+	ones  int
+}
+
 func main() {
+	var powerConsumption []Rates
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatalln("Something went wrong while reading the file")
@@ -16,10 +23,36 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	var bits []string
-
 	for scanner.Scan() {
-		bits = append(bits, scanner.Text())
+		bits := scanner.Text()
+
+		for idx, char := range bits {
+			if len(powerConsumption) <= idx {
+				powerConsumption = append(powerConsumption, Rates{})
+			}
+			if char == '0' {
+				powerConsumption[idx].zeros += 1
+
+			} else {
+				powerConsumption[idx].ones += 1
+			}
+		}
 	}
-	fmt.Println(len(bits))
+
+	gamaRate, epsilonRate := "", ""
+
+	for _, powerData := range powerConsumption {
+		if powerData.zeros > powerData.ones {
+			gamaRate += "0"
+			epsilonRate += "1"
+		} else {
+			gamaRate += "1"
+			epsilonRate += "0"
+		}
+	}
+
+	gama, _ := strconv.ParseInt(gamaRate, 2, 64)
+	epsilon, _ := strconv.ParseInt(epsilonRate, 2, 64)
+
+	fmt.Println(gama * epsilon)
 }
